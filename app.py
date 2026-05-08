5e856fb920e20f5e5c1318a88a4170a29f5a12c4
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2026-05-08 09:31:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello from Docker CI/CD 🚀")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from AWS EC2 + Docker + GitHub Actions 🚀"
+
+app.run(host='0.0.0.0', port=5000)
